067d08fb6811767cb41f68108ce12e7c181f331a
epeters3 <evanpeterson17@gmail.com>
2019-08-31 02:31:56 -0600

    Streamline dataset loading

--- AutoTensor/data_mgmt/data_loader.py
+++ AutoTensor/data_mgmt/data_loader.py
@@ -1,110 +1,95 @@
-from __future__ import print_function
 import json
 from collections import namedtuple
+import re
+from typing import NamedTuple
 
-import arff
+from scipy.io import arff
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, LabelBinarizer, LabelEncoder
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 
 from AutoTensor.data_mgmt.data_shaper import normalize
-# from AutoTensor.data.mushrooms.UnpackMushrooms import unpack as unpack_mushrooms
-
-Data = namedtuple("Data", [
-    "train_data", "train_labels", "val_data", "val_labels", "test_data",
-    "test_labels"
-])
 
 
-def load_json_str(file_path):
-    """Returns the json string from a .json file found at file_path"""
-    with open(file_path) as f:
-        data = json.load(f)
-        return json.dumps(data)
+class DataSet(NamedTuple):
+    train_data: pd.DataFrame
+    train_labels: pd.Series
+    val_data: pd.DataFrame
+    val_labels: pd.Series
+    test_data: pd.DataFrame
+    test_labels: pd.Series
 
 
-def load_arff(file_path, ):
-    with open(file_path) as f:
-        dataset = arff.load(f, encode_nominal=True)
-        return np.array(dataset["data"]), dataset
-
-
-def split_last_col(data):
-    features = data[:, :-1]
-    labels = data[:, -1]
-    return features, labels
-
-
-def one_hot_encode(vector):
-    classes = np.unique(vector)
-    # Source: https://stackoverflow.com/questions/29831489/convert-array-of-indices-to-1-hot-encoded-numpy-array
-    encoded = np.zeros((vector.shape[0], classes.size))
-    encoded[np.arange(vector.shape[0]), vector.astype(int)] = 1
-    return encoded
-
-
-def split_dataset(data,
-                  test_ratio,
-                  val_ratio,
-                  should_one_hot_encode,
-                  labels=None):
+def prepare_dataset(
+    X: pd.DataFrame, y: pd.Series, test_ratio: float, val_ratio: float
+) -> DataSet:
     """
     Splits data and labels into training, validation, and test sets.
-    If labels=None, it is assumed the last column of data contains the labels
     """
     if test_ratio < 0 or test_ratio > 1 or val_ratio < 0 or val_ratio > 1:
-        raise Exception("train_ratio must be between 0 and 1")
-    if test_ratio + val_ratio > 1:
-        raise Exception(
-            "The sum of test_ratio and val_ratio cannot be greater than 1")
+        raise ValueError("train_ratio must be between 0 and 1")
+    if not test_ratio + val_ratio < 1:
+        raise ValueError("the sum of test_ratio and val_ratio must be less than 1")
 
-    np.random.shuffle(data)
-    num_instances = data.shape[0]
+    num_instances = X.shape[0]
+    X, y = shuffle(X, y)
 
-    test_end = int(num_instances * test_ratio)
-    val_end = test_end + int(num_instances * val_ratio)
-    if labels is None:
-        data, labels = split_last_col(data)
-    data = normalize(data)
+    normalizer = StandardScaler()
+    X = normalizer.fit_transform(X)
 
-    if should_one_hot_encode:
-        labels = one_hot_encode(labels)
+    # One hot encode y
+    encoder = LabelEncoder()
+    binarizer = LabelBinarizer()
+    y = encoder.fit_transform(y)
+    y = binarizer.fit_transform(y)
+    print(f"one hot encoded y: {y}")
 
-    test_data = data[:test_end, :]
-    test_labels = labels[:test_end]
+    rest_ratio = val_ratio + test_ratio
+    train_data, rest_data, train_labels, rest_labels = train_test_split(
+        X, y, train_size=1 - rest_ratio, test_size=rest_ratio
+    )
 
-    val_data = data[test_end:val_end, :]
-    val_labels = labels[test_end:val_end]
+    val_data, test_data, val_labels, test_labels = train_test_split(
+        rest_data,
+        rest_labels,
+        train_size=val_ratio / rest_ratio,
+        test_size=test_ratio / rest_ratio,
+    )
 
-    train_data = data[val_end:, :]
-    train_labels = labels[val_end:]
-    print("train_data[0:2]={}\ntrain_labels[0:2]={}".format(
-        train_data[0:2], train_labels[0:2]))
+    print(
+        "train_data[0:2]={}\ntrain_labels[0:2]={}".format(
+            train_data[0:2], train_labels[0:2]
+        )
+    )
 
-    return Data(train_data, train_labels, val_data, val_labels, test_data,
-                test_labels)
+    return DataSet(
+        train_data, train_labels, val_data, val_labels, test_data, test_labels
+    )
 
 
-def get_arff_dataset(file_path, test_ratio, val_ratio):
-    data, data_with_meta = load_arff(file_path)
-    data = split_dataset(data, test_ratio, val_ratio, True)
-    num_classes = len(data_with_meta["attributes"][-1][1])
-    print("arff dataset meta data:\n{}".format(data_with_meta["attributes"]))
-    print("num_classes: {}".format(num_classes))
+def get_arff_dataset(file_path, target_index, test_ratio, val_ratio):
+    data_arr, _ = arff.loadarff(file_path)
+    data_arr = pd.DataFrame(data_arr)
+    x_indices = [i for i in range(data_arr.shape[1]) if i != target_index]
+    target_col_name = data_arr.columns[target_index]
+    X = data_arr.drop(columns=target_col_name)
+    y = pd.Series(data_arr[target_col_name], dtype="category")
+    data = prepare_dataset(X, y, test_ratio, val_ratio)
+    num_classes = y.nunique()
     return data, num_classes
 
 
-# def get_my_mushrooms():
-#     mushrooms = unpack_mushrooms()
-#     image_arrays = np.array([mushroom.image_data for mushroom in mushrooms])
-#     family_list = [
-#         "Pluteaceae", "Boletaceae", "Cortinariaceae", "Russulaceae",
-#         "Agaricaceae"
-#     ]
-#     labels = np.array(
-#         [1 if mushroom.family in family_list else 0 for mushroom in mushrooms])
-#     data = split_dataset(image_arrays, 0.15, 0.15, True, labels)
-#     num_classes = data.train_labels.shape[1]
-#     return data, num_classes
+def get_file_ext(file_path):
+    file_ext_regex = r"\.[^\.]+$"
+    return re.search(file_ext_regex, file_path)[0]
 
 
-def get_my_arff_dataset():
-    return get_arff_dataset("AutoTensor/data/gamma.arff", 0.15, 0.15)
+file_ext_to_loader = {".arff": get_arff_dataset}
+
+
+def load_dataset_file(path, target_index):
+    ext = get_file_ext(path)
+    # TODO: add test_ratio and val_ratio as CLI args
+    return file_ext_to_loader[ext](path, target_index, 0.15, 0.15)
