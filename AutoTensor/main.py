d8fe995554948632ee5e961391d8d6fe1183b55b
epeters3 <evanpeterson17@gmail.com>
2019-08-31 02:29:33 -0600

    Add CLI interface for parsing files (only arff supported currently)

--- AutoTensor/main.py
+++ AutoTensor/main.py
@@ -1,24 +1,48 @@
-from __future__ import absolute_import, division, print_function
 import json
+from argparse import ArgumentParser
 
 import tensorflow as tf
 import pprint
 
 from AutoTensor.q_learning.QLearner import QLearner
-from AutoTensor.q_learning.tensor_q_mapper import actions, starting_state, compose_get_state, compose_get_reward
-from AutoTensor.data_mgmt.data_loader import get_my_arff_dataset, load_json_str
+from AutoTensor.q_learning.tensor_q_mapper import (
+    actions,
+    starting_state,
+    compose_get_state,
+    compose_get_reward,
+)
+from AutoTensor.data_mgmt.data_loader import load_dataset_file
 
 
-def find_optimal_model(get_dataset):
-    data, num_classes = get_dataset()
+def get_cli_args():
+    parser = ArgumentParser(
+        description="Conduct Neural Architecture Search on a dataset file."
+    )
+    parser.add_argument(
+        "--path",
+        "-p",
+        type=str,
+        required=True,
+        help="The path to the dataset file to train on.",
+    )
+    parser.add_argument(
+        "--target-index",
+        "-t",
+        type=int,
+        default=-1,
+        help=(
+            "The 0-based index of the target column in the dataset file. "
+            "Negative indexing is also supported. If not supplied, it is "
+            "assumed the last column is the target."
+        ),
+    )
+    return parser.parse_args()
+
+
+def find_optimal_model(data, num_classes):
 
     get_state = compose_get_state()
     get_reward = compose_get_reward(data, num_classes)
-
-    # acc = get_reward(starting_state)
-    # print(
-    #     "======== FINISHED WITH TEST OF CURRYING AND TENSOR FLOW - ACCURACY IS {} ========"
-    #     .format(acc))
 
     qlearner = QLearner(
         discount=0.9,
@@ -27,10 +51,13 @@ def find_optimal_model(get_dataset):
         get_reward=get_reward,
         starting_state=starting_state,
         actions=actions,
-        report_file_path="AutoTensor/reports/q-learner-results.json")
+        report_file_path="AutoTensor/reports/q-learner-results.json",
+    )
 
     qlearner.find_state_with_best_q()
 
 
 if __name__ == "__main__":
-    find_optimal_model(get_my_arff_dataset)
+    args = get_cli_args()
+    data, num_classes = load_dataset_file(args.path, args.target_index)
+    find_optimal_model(data, num_classes)
